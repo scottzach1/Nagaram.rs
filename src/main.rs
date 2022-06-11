0bd0c4459fe6636cf34021e24ec84f915a68f245
Zac Scott <zac.scott11@gmail.com>
2022-06-12 09:08:52 +1200

    Implement traversal

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+extern crate core;
+
 use std::{env, fs};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::str::{Lines};
 
-#[derive(Debug)]
 struct Node {
     path: String,
-    root: bool,
+    word: bool,
     children: HashMap<char, Node>,
 }
 
@@ -13,14 +14,14 @@ impl Node {
     fn default() -> Node {
         Node {
             path: format!(""),
-            root: false,
+            word: false,
             children: HashMap::new(),
         }
     }
     fn from_str(str: String) -> Node { Node { path: str, ..Node::default() } }
 
     fn ingest(self, word: String) -> Node {
-        if word.is_empty() { return self; }
+        if word.is_empty() { return Node { word: true, ..self }; }
         let (c, rest) = get_c(&word);
 
         let mut children = self.children;
@@ -32,14 +33,61 @@ impl Node {
 
         Node {
             path: self.path,
+            word: self.word,
             children,
             ..Node::default()
         }
     }
+
+    fn anagrams_helper(&self, word: String, root: &Node) -> Vec<String> {
+        let mut results = vec![];
+
+        if word.is_empty() {
+            if self.word {
+                // Base case 1: Perfect match
+                results.push(self.path.clone());
+            }
+            return results;
+        }
+
+        if self.word {
+            // Base case 2: Hit end of path but still more string
+            let remainder_results = root.anagrams_helper(word.clone(), root);
+            results.extend(remainder_results.into_iter().map(|w| format!("{} {}", self.path, w)));
+        }
+
+        // Keep recursing deeper
+        let char_set: HashSet<char> = HashSet::from_iter(word.chars());
+        char_set.into_iter().fold(results, |mut results, c| {
+            let rest = remove_c(&word, c).unwrap();
+            match self.children.get(&c) {
+                Some(child) => { results.extend(child.anagrams_helper(rest, root)) }
+                None => {}
+            };
+            results
+        })
+    }
+
+    fn anagrams(&self, word: String) -> Vec<String> {
+        self.anagrams_helper(word, &self)
+    }
 }
 
 fn parse(iterator: Lines) -> Node {
-    iterator.fold(Node::default(), |n, line| n.ingest(line.to_string()))
+    iterator.fold(Node::default(), |n, line| n.ingest(line.to_string().to_lowercase()))
+}
+
+/// Remove the first instance of a character from a string
+fn remove_c(subject: &String, char: char) -> Option<String> {
+    let index = subject.find(char);
+    match index {
+        Some(i) => {
+            let mut subject = subject.clone();
+            subject.remove(i);
+            Some(subject)
+        }
+        None => None
+    }
 }
 
 /// Extract the next character from a String, returning bot the next char and remainder.
@@ -51,12 +99,37 @@ fn get_c(s: &String) -> (char, String) {
     }
 }
 
+fn remove_whitespace(s: &str) -> String {
+    s.chars().filter(|c| !c.is_whitespace()).collect()
+}
+
+fn get_anagrams(target: String) {
+    let target = remove_whitespace(&target).to_lowercase();
+    let words = fs::read_to_string("words.txt").expect("Could not locate dictionary");
+    let root = parse(words.lines());
+
+    let anagrams = root.anagrams(target);
+    anagrams.iter().for_each(|w| println!("- {:#?}", w));
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("Arguments: {:?}", args);
 
-    let dictionary = fs::read_to_string("words.txt")
-        .expect("Could not locate dictionary");
+    let target = match args.len() {
+        0..=1 => {
+            println!("Missing second word argument!");
+            None
+        }
+        2 => Some(&args[1]),
+        n => {
+            println!("Unexpected arguments! {:#?}", &args[2..n]);
+            Some(&args[1])
+        }
+    };
 
-    parse(dictionary.lines());
+    match target {
+        Some(target) => { get_anagrams(target.clone()) }
+        None => {}
+    }
 }
