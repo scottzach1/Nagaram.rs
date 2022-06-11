02076f300556cf6401d62ff83e5bbc5ad69aa5b4
Zac Scott <zac.scott11@gmail.com>
2022-06-12 05:06:17 +1200

    Load dictionary file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use std::{env};
+use std::{env, fs};
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("Arguments: {:#?}", args);
+    println!("Arguments: {:?}", args);
+
+    let dictionary = fs::read_to_string("words.txt")
+        .expect("Could not locate dictionary");
+
+    println!("Dictionary:");
+    dictionary.lines().into_iter().for_each(|word|println!("- {}", word));
 }
