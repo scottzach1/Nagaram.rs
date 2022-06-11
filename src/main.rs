9123a51d9a6a0de82168ffa79c8b8e30e13515fb
Zac Scott <zac.scott11@gmail.com>
2022-06-12 06:22:50 +1200

    Add ingest() method

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,61 @@
 use std::{env, fs};
-use std::iter::Map;
-use std::str::Lines;
+use std::collections::HashMap;
+use std::str::{Lines};
 
 #[derive(Debug)]
 struct Node {
     path: String,
-    parent: Option<Box<Node>>,
-    children: Option<Map<String, Box<Node>>>,
+    root: bool,
+    children: HashMap<char, Node>,
 }
 
 impl Node {
-    fn get_root(self) -> Node {
-        match self.parent {
-            Some(parent) => parent.get_root(),
-            None => self
+    fn default() -> Node {
+        Node {
+            path: format!(""),
+            root: false,
+            children: HashMap::new(),
+        }
+    }
+
+    fn ingest(self, word: String) -> Node {
+        if word.is_empty() { return self }
+
+        let (c, rest) = get_c(word);
+        let child_default = Node { path: format!("{}{}", &self.path, c), ..Node::default() };
+
+        let mut children = self.children;
+        let child = children
+            .remove(&c).unwrap_or(child_default)
+            .ingest(rest);
+        children.insert(c, child);
+
+        Node {
+            path: self.path,
+            children,
+            ..Node::default()
         }
     }
 }
 
-fn parse(iterator: Lines) {
+fn parse(_iterator: Lines) -> Node {
     println!("Dictionary:");
-    iterator.for_each(|word|println!("- {}", word));
+    // iterator.for_each(|word|println!("- {}", word));
+
+    let root = Node::default().ingest(format!("abc")).ingest(format!("ad"));
+    println!("{:#?}", root);
+    root
 }
 
+/// Extract the next character from a String, returning bot the next char and remainder.
+/// Throws a `SyntaxError` on end of string.
+fn get_c(s: String) -> (char, String) {
+    let mut chars = s.chars();
+    match chars.next() {
+        Some(c) => (c, chars.collect()),
+        None => panic!("unexpected end of string"),
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
